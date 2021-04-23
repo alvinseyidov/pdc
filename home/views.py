22d54f5813addae6c3ee4318529c9eb36b92b9a7
Elvin Seyidzade <alvinseyidzade@gmail.com>
2021-04-24 02:54:46 +0400

    update

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from portfolio.models import Category, Project
 from haqqimizda.models import FAQ
+from xidmetler.models import Xidmetler
 
 # Create your views here.
 
@@ -9,10 +10,12 @@ def home(request):
     project_categories = Category.objects.all()
     projects = Project.objects.all()
     faq = FAQ.objects.all()
+    xidmetler = Xidmetler.objects.all()
 
     context = {
         "project_categories": project_categories,
         "projects": projects,
-        "faq": faq
+        "faq": faq,
+        "xidmetler": xidmetler
     }
     return render(request, "home.html", context)
--- xidmetler/models.py
+++ xidmetler/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Xidmetler(models.Model):
+    name = models.CharField(verbose_name='Xidmet adı', max_length=256, null=True, blank=True)
+   
+    def __str__(self):
+        return self.name
