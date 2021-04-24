87cc101493fe82d2544e01b8d37af6e06dd7e599
Elvin Seyidzade <alvinseyidzade@gmail.com>
2021-04-24 15:02:36 +0400

    update

--- pdc/settings.py
+++ pdc/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'pdc.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -101,7 +101,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
 }}
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- xidmetler/models.py
+++ xidmetler/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-
+from ckeditor.fields import RichTextField
 # Create your models here.
 
 class Xidmetler(models.Model):
     name = models.CharField(verbose_name='Xidmet adı', max_length=256, null=True, blank=True)
+    image = models.ImageField(verbose_name='Şəkil', null=True, blank=True)
+    description = RichTextField(verbose_name='Haqqında Ətraflı Məlumat', blank=True, null=True)
    
     def __str__(self):
         return self.name
