adea56a20cd23d329d3afeeeb51afb79d7183733
Elvin Seyidzade <alvinseyidzade@gmail.com>
2021-04-28 19:31:21 +0400

    update

--- xidmetler/models.py
+++ xidmetler/models.py
@@ -4,6 +4,7 @@ from ckeditor.fields import RichTextField
 
 class Xidmetler(models.Model):
     name = models.CharField(verbose_name='Xidmet adı', max_length=256, null=True, blank=True)
+    iconimage = models.ImageField(verbose_name='Icon', null=True, blank=True)
     image = models.ImageField(verbose_name='Şəkil', null=True, blank=True)
     description = RichTextField(verbose_name='Haqqında Ətraflı Məlumat', blank=True, null=True)
    
