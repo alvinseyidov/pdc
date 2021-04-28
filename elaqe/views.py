0809578e463818e7f6b89d228b357d529573fe0b
Elvin Seyidzade <alvinseyidzade@gmail.com>
2021-04-28 20:20:35 +0400

    update

--- elaqe/views.py
+++ elaqe/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from xidmetler.models import Xidmetler
+from haqqimizda.models import Partnyorlar
 # Create your views here.
 
 def elaqe(request):
     xidmetler = Xidmetler.objects.all()
+    partnyorlar = Partnyorlar.objects.all()
     context = {
-        "xidmetler": xidmetler
+        "xidmetler": xidmetler,
+        "partnyorlar": partnyorlar
     }
     return render(request, "elaqe.html",context)
+
+
+
+   
