6b49bb3007df6ce172aa054038a845d641e30445
alvinseyidov <alvinseyidov@gmail.com>
2021-05-22 01:57:52 +0400

    update

---
+++ static/js/main2.js
@@ -0,0 +1,48 @@
+// MAIN.JS
+/*-------------------------------------------------------------------------------------------------------------------------------*/
+//This is main JS file that contains custom JS scipts and initialization used in this template */
+
+/*global $:false */
+/*global window: false */
+
+(function(){
+    var $ = jQuery.noConflict();
+  "use strict";
+//var $ = jQuery.noConflict();
+
+// scroll to top
+
+
+
+
+
+
+/* Loader */
+$(window).load(function() {
+    $(".loader-item").delay(800).fadeOut();
+    $("#loader, #loader2, #loader3").delay(1300).fadeOut("slow");
+});
+
+    
+})();
+
+
+
+
+var $ = jQuery.noConflict();
+
+var MAJESTY = MAJESTY || {};
+
+
+
+
+
+
+
+/* ==  
+    Googler Map   
+==*/
+
+// Create and Initialise the Map (required) our google map below
+
+   
